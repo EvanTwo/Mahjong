8c5081f2a777d1c4f903cfcac7a21e128699b3b6
EvanTwo <cht773@student.cis.edu.hk>
2024-02-25 21:01:34 +0800

    Updated MahjongTile.java to have enumerators and getters.

---
+++ src/MahjongTile.java
@@ -0,0 +1,38 @@
+public class MahjongTile {
+    enum Suit
+        {
+            tenThousand, stick, circle, wind, dragon, flower, season; //萬、索、筒、風、龍、花、季
+            private static final Suit[] suits = Suit.values();
+            public static Suit getSuit(int i){
+                return Suit.suits[i];
+            }
+        }
+    enum Value
+        {
+            Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, East, South, West, North, Red, Green, White, Spring, Summer, Autumn, Winter, Plum, Orchid, Bamboo, Chrysanthemum;
+            private static final Value[] values = Value.values();
+            public static Value getSuit(int i){
+                return Value.values[i];
+            }
+        }
+
+    private final Suit suit;
+    private final Value value;
+
+    public MahjongTile(final Suit suit, final Value value){
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public Suit getSuit() {
+        return this.suit;
+    }
+
+    public Value getValue(){
+        return this.value;
+    }
+
+    public String toString(){
+        return suit + "_" + value;
+    }
+}
