5faa1a150c8a51e14f34837945e17a043c18e11b
EvanTwo <cht773@student.cis.edu.hk>
2024-02-26 08:32:14 +0800

    Added a MahjongDeck.java file.

---
+++ src/MahjongDeck.java
@@ -0,0 +1,18 @@
+
+    /* Mahjong has 144 cards, consisting of the three main suits, which are, ten thousands, sticks, and cirlces.
+    There are also winds, dragons, flowers, and seasons.
+    There are 4 1-9 cards for the three main suits, four of each of the four winds, four of each of the three dragons,
+    four flowers, and four seasons.
+     */
+
+
+    public class MahjongDeck {
+        private MahjongTile[] tiles;
+        private int tilesInDeck;
+
+        public MahjongDeck(){
+                tiles = new MahjongTile[144];
+
+
+        }
+}
