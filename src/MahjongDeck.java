e348230aaf48e16f00987e4c2e12feaaaf99788f
EvanTwo <cht773@student.cis.edu.hk>
2024-02-28 21:55:07 +0800

    Simplified/optimised some getters.

--- src/MahjongDeck.java
+++ src/MahjongDeck.java
@@ -1,95 +1,94 @@
 
-    /* Mahjong has 144 cards, consisting of the three main suits, which are, ten thousands, sticks, and cirlces.
-    There are also winds, dragons, flowers, and seasons.
-    There are 4 1-9 cards for the three main suits, four of each of the four winds, four of each of the three dragons,
-    four flowers, and four seasons.
-     */
+/* Mahjong has 144 cards, consisting of the three main suits, which are, ten thousands, sticks, and cirlces.
+There are also winds, dragons, flowers, and seasons.
+There are 4 1-9 cards for the three main suits, four of each of the four winds, four of each of the three dragons,
+four flowers, and four seasons.
+ */
 
 
-    import javax.swing.*;
-    import java.util.ArrayList;
+import javax.swing.*;
+import java.util.ArrayList;
+public class MahjongDeck {
+    private ArrayList<MahjongTile> tiles;
 
-    public class MahjongDeck {
-        private MahjongTile[] tiles;
-        private int tilesInDeck;
+       /* public MahjongDeck() { // what does this block do?
+            deck = new MahjongTile[144]; // 144 tiles in a deck
+        }*/
 
-        public MahjongDeck() {
-            tiles = new MahjongTile[144]; // 144 tiles in a deck
-        }
-        public void resetDeck(){
+    public void resetDeck(){
 
-            MahjongTile.Suit[] suits = MahjongTile.Suit.values();
-            MahjongTile.Value[] values = MahjongTile.Value.values();
-            tilesInDeck = 0;
+        MahjongTile.Suit[] suits = MahjongTile.Suit.values();
+        MahjongTile.Value[] values = MahjongTile.Value.values();
 
-            // 1-9 for the three main suits
-            for(int i = 0; i<3; i++){ // tenThousand, stick, circle in the enumeration of suits
-                MahjongTile.Suit suit = suits[i];
+        // 1-9 for the three main suits
+        for(int i = 0; i<3; i++){ // tenThousand, stick, circle in the enumeration of suits
+            MahjongTile.Suit suit = suits[i];
 
-                for(int j = 0;j <9; j++){ // 1-9 in the enumeration of values
-                    tiles[tilesInDeck++] = new MahjongTile(suit, MahjongTile.Value.getValue(j)); // 4 of each of the 1-9 cards
-                    tiles[tilesInDeck++] = new MahjongTile(suit, MahjongTile.Value.getValue(j));
-                    tiles[tilesInDeck++] = new MahjongTile(suit, MahjongTile.Value.getValue(j));
-                    tiles[tilesInDeck++] = new MahjongTile(suit, MahjongTile.Value.getValue(j));
-                }
-            }
-
-            // winds
-            for(int i = 0; i<4; i++){
-                tiles[tilesInDeck++] = new MahjongTile(suits[3], values[i+10]); // 4 of each of the four winds
-                tiles[tilesInDeck++] = new MahjongTile(suits[3], values[i+10]);
-                tiles[tilesInDeck++] = new MahjongTile(suits[3], values[i+10]);
-                tiles[tilesInDeck++] = new MahjongTile(suits[3], values[i+10]);
-            }
-
-            // dragons
-            for(int i = 0; i<3; i++){
-                tiles[tilesInDeck++] = new MahjongTile(suits[4], values[i+14]); // 4 of each of the three dragons
-                tiles[tilesInDeck++] = new MahjongTile(suits[4], values[i+14]);
-                tiles[tilesInDeck++] = new MahjongTile(suits[4], values[i+14]);
-                tiles[tilesInDeck++] = new MahjongTile(suits[4], values[i+14]);
-            }
-
-            // flowers
-            for(int i = 0; i<4; i++){
-                tiles[tilesInDeck++] = new MahjongTile(suits[5], values[i+17]); // 1 of each of the four flowers
-            }
-
-            // seasons
-            for(int i = 0; i<4; i++){
-                tiles[tilesInDeck++] = new MahjongTile(suits[6], values[i+21]); // 1 of each of the four seasons
+            for(int j = 0;j <9; j++){ // 1-9 in the enumeration of values
+                tiles.add(new MahjongTile(suit, MahjongTile.Value.getValue(j))); // 4 of each of the 1-9 cards
+                tiles.add(new MahjongTile(suit, MahjongTile.Value.getValue(j)));
+                tiles.add(new MahjongTile(suit, MahjongTile.Value.getValue(j)));
+                tiles.add(new MahjongTile(suit, MahjongTile.Value.getValue(j)));
             }
         }
-        public boolean isEmpty(){
-            return tilesInDeck == 0;
+
+        // winds
+        for(int i = 0; i<4; i++){
+            tiles.add(new MahjongTile(suits[3], values[i+10])); // 4 of each of the four winds
+            tiles.add(new MahjongTile(suits[3], values[i+10]));
+            tiles.add(new MahjongTile(suits[3], values[i+10]));
+            tiles.add(new MahjongTile(suits[3], values[i+10]));
         }
 
-        public void shuffle(){
-            for(int i = 0;i<tiles.length;i++){
-                int randomIndex = (int)(Math.random()*tiles.length);
-                MahjongTile temp = tiles[i];
-                tiles[i] = tiles[randomIndex];
-                tiles[randomIndex] = temp;
-            }
-        }
-        // draw a card from the deck
-        public MahjongTile drawTile() throws IllegalArgumentException{
-            if(isEmpty()){
-                throw new IllegalArgumentException("Deck is empty");
-            }
-            return tiles[--tilesInDeck];
+        // dragons
+        for(int i = 0; i<3; i++){
+            tiles.add(new MahjongTile(suits[4], values[i+14])); // 4 of each of the three dragons
+            tiles.add(new MahjongTile(suits[4], values[i+14]));
+            tiles.add(new MahjongTile(suits[4], values[i+14]));
+            tiles.add(new MahjongTile(suits[4], values[i+14]));
         }
 
-        public MahjongTile drewFlowerSeason{
-            MahjongTile[] temp = new MahjongTile[tiles.length-1];
-            int length = temp.length;
-            MahjongTile replacement = temp[length-1];
-            // fix this
+        // flowers
+        for(int i = 0; i<4; i++){
+            tiles.add(new MahjongTile(suits[5], values[i+17])); // 1 of each of the four flowers
         }
+
+        // seasons
+        for(int i = 0; i<4; i++){
+            tiles.add(new MahjongTile(suits[6], values[i+21])); // 1 of each of the four seasons
+        }
+    }
+
+
+    public void shuffle(){
+        for(int i = 0;i<tiles.size();i++){
+            int randomIndex = (int)(Math.random()*tiles.size());
+            MahjongTile temp = tiles.get(i);
+            tiles.set(i, tiles.get(randomIndex));
+            tiles.set(randomIndex, temp);
+        }
+    }
+    // draw a card from the deck
+    public MahjongTile drawTile() throws IllegalArgumentException{
+        if(tiles.isEmpty()){
+            throw new IllegalArgumentException("Deck is empty");
+        }
+        MahjongTile tile = tiles.getFirst(); // draw the top card from the deck
+        tiles.removeFirst(); // remove the top card from the deck as it has been drawn
+        return tile;
+    }
+
+    public MahjongTile drewFlowerSeason() throws IllegalArgumentException{
+        if(tiles.isEmpty()) {
+            throw new IllegalArgumentException("Deck is empty");
+        }
+        MahjongTile tile = tiles.getLast(); // draw the last card from the deck replacing a flower or season
+        tiles.removeLast();
+        return tile;
+    }
 
 
         /*
-        // top card of the deck
         public ImageIcon drawTileImage() throws IllegalArgumentException{
             if(isEmpty()){
                 throw new IllegalArgumentException("Deck is empty");
@@ -97,4 +96,4 @@
             return new ImageIcon(tiles(--tilesInDeck).toString()) + ".png";
         }
         */
-    }
+}
